8b341fcb7101956e181572cc64e9da44573a6203
Home <sairaman28@gmail.com>
2021-09-25 16:54:29 +0530

    Version 1.0 - Beginning

---
+++ Snake.hpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+class Snake
+{
+    public:
+        Snake()
+        {
+            cout<<"New Game Started";
+        }
+        ~Snake()
+        {
+            cout<<"Closing the Game";
+        }
+        void Play()
+        {
+
+        }
+};
--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
-#include<iostream>
-using namespace std;
+
+#include "Snake.hpp"
 int main()
 {
-    cout << "Snake Game"<<endl;
+    char start_choice,end_choice;
+    cout<<"Welcome to my Game"<<endl;
+    cout<<"Do you want to start a new game"<<endl;
+    cin>>start_choice;
+    if(start_choice == 'y' )
+    {
+        Snake s;
+    }
+  
+    cout<<"Press 'y' to exit "<< endl;
+    while(end_choice != 'y' )
+    {
+        cin>>end_choice;  
+    };
     return 0;
 }
